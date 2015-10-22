e979bb5a0d76b2822dca451be9f0dfd9f23a115b
Zachary Schweikert <zachary.schweikert@west-mec.com>
2015-10-22 15:26:35 -0700

    YYYyayayayayyyy

--- script.js
+++ script.js
@@ -1,23 +1,7 @@
 
 
 
-function changeText(id) {
-    id.innerHTML = "<div id='thing2'> This is a new thing</div>";
-}
-function changeText1(id) {
-    if (foodName1 == 1) {
-        id.innerHTML = "<div id='foodName1'> s </div>";
-    }
-}
-
-function popup(id) {
-    $(document).ready(function() {
-        $(".button").click(function() {
-
-            var myNumber = 19.2;
-            var myAnswer = prompt ("How many miles did you travel?");
-            document.write( " Your Carbon Foot print is:" + myAnswer * myNumber + "lbs" );
-
-        });
-    });
+function newRow()  {
+    var foodName = document.getElementById('foodName').value;
+    document.getElementById('newRow').innerHTML = "<tr><td>foodName</td></tr>"
 }
