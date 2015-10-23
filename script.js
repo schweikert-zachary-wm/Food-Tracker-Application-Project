770f17c38316018146625ea9e071236e7389fc8e
Zachary Schweikert <zachary.schweikert@west-mec.org>
2015-10-22 17:40:38 -0700

    It now makes new rows but is still not completed.

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 
 
 
-function newRow()  {
+//function newRow()  {
+  //  var foodName = document.getElementById('foodName').value;
+    //  document.getElementById('newRow').innerHTML = "<h2 id='foodName2'> foodName2 goes here </h2><br><br><div id='newRow'> div  stuff</div>";
+    //  document.getElementById('foodName2').innerHTML = foodName;
+
+//}
+
+
+
+function newRow (){
+
     var foodName = document.getElementById('foodName').value;
-    document.getElementById('newRow').innerHTML = "<tr><td>foodName</td></tr>"
+    var newDiv = document.createElement("h2 id='foodName1");
+    var newContent = document.createTextNode(foodName);
+    newDiv.appendChild(newContent); //add the text node to the newly created div.
+
+    // add the newly created element and its content into the DOM
+    var currentDiv = document.getElementById("foodName1");
+    document.body.insertBefore(newDiv, currentDiv);
+
 }
