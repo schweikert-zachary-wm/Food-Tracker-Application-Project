689e939a0e0c98b42979f6f9049323562445a67c
Zachary Schweikert <zachary.schweikert@west-mec.org>
2015-10-22 18:24:19 -0700

    Lines completed. They add a new line with whatever is entered in the boxes onto a new line.

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
 function newRow (){
 
     var foodName = document.getElementById('foodName').value;
-    var newDiv = document.createElement("h2 id='foodName1");
+    var newDiv = document.createElement("h2");
     var newContent = document.createTextNode(foodName);
     newDiv.appendChild(newContent); //add the text node to the newly created div.
 
@@ -21,4 +21,31 @@ function newRow (){
     var currentDiv = document.getElementById("foodName1");
     document.body.insertBefore(newDiv, currentDiv);
 
+
+    var description = document.getElementById('description').value;
+    var newDiv = document.createElement("h2");
+    var newContent = document.createTextNode(description);
+    newDiv.appendChild(newContent); //add the text node to the newly created div.
+
+    // add the newly created element and its content into the DOM
+    var currentDiv = document.getElementById("foodName1");
+    document.body.insertBefore(newDiv, currentDiv);
+
+    var purchase = document.getElementById('purchase').value;
+    var newDiv = document.createElement("h2");
+    var newContent = document.createTextNode(purchase);
+    newDiv.appendChild(newContent); //add the text node to the newly created div.
+
+    // add the newly created element and its content into the DOM
+    var currentDiv = document.getElementById("foodName1");
+    document.body.insertBefore(newDiv, currentDiv);
+
+    var expire = document.getElementById('expire').value;
+    var newDiv = document.createElement("h2");
+    var newContent = document.createTextNode(expire);
+    newDiv.appendChild(newContent); //add the text node to the newly created div.
+
+    // add the newly created element and its content into the DOM
+    var currentDiv = document.getElementById("foodName1");
+    document.body.insertBefore(newDiv, currentDiv);
 }
