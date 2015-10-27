221f978eaa1a4a3f2347b1550ca5d100d0f31ff0
schweikert-zachary-wm <zachary.schweikert@west-mec.org>
2015-10-26 20:28:58 -0700

    The script is done.
    The expiration date thing works perfectly. Don't touch it.

--- script.js
+++ script.js
@@ -28,13 +28,8 @@ function newRow (){
     document.body.insertBefore(newDiv, currentDiv);
 
 // make an if statement to make sure the date is not null and then run expire1();
-    var expire = document.getElementById('expire').value;
-    var newDiv = document.createElement("h2");
-    var newContent = document.createTextNode(expire);
-    newDiv.appendChild(newContent); //add the text node to the newly created div.
-    // add the newly created element and its content into the DOM
-    var currentDiv = document.getElementById("foodName1");
-    document.body.insertBefore(newDiv, currentDiv);
+
+    expire1();
 
 
 
@@ -45,53 +40,80 @@ function newRow (){
 
 
 
-
-function expired () {
-var day1 = new Date();
-    var day = day1.getDate();
-var month2 = new Date();
-    var month1 = month2.getMonth();
-    var month = month1 + 1;
-var year1 = new Date();
-    var year = year1.getFullYear();
-var date = year + "-" + month + "-" + day;
-    alert("the date is" + date);
+function expire1 () {
 
 
+    var expire = document.getElementById('expire').value;
+    var expire2 = expire.substring(0,4);
+    if (expire2 == 2015) {
 
+        expire1a();
+    }
+    if (expire2 > 2015) {
+    var newDiv = document.createElement("h2");
+        var newContent = document.createTextNode(expire);
+        newDiv.appendChild(newContent); //add the text node to the newly created div.
+        // add the newly created element and its content into the DOM
+        var currentDiv = document.getElementById("foodName1");
+        document.body.insertBefore(newDiv, currentDiv);
+}
+    if (expire2 < 2015){
+        var newDiv = document.createElement("h4");
+        var newContent = document.createTextNode(expire);
+        newDiv.appendChild(newContent); //add the text node to the newly created div.
+        // add the newly created element and its content into the DOM
+        var currentDiv = document.getElementById("foodName1");
+        document.body.insertBefore(newDiv, currentDiv);
+    }
 
-    //Trigger function to check all dates before current date.
-    while (year != 2000) {
-        var month3 = 12;
-        while (month3 != 0) {
-            var day2 = 31;
-            while (day2 != 1) {
-                var date = year + "-" + month3 + "-" + day2;
-                alert("the date is 2 " + date);
-                var doc1 = document.getElementById("body").innerHTML;
-                var doc2 = doc1.replace("<h2>" + date + "</h2>", "<h2> EXPIRED </h2>");
-                document.getElementById("body").innerHTML = doc2;
-                day2 = day2 - 1;
-            }
-            month3 = month3 -1;
-        }
-        year = year - 1;
+}
+function expire1a() {
+    var expire = document.getElementById('expire').value;
+    var expire1a = expire.substring(5,7);
+    var d = new Date();
+    var d2 = d.getMonth();
+    var today = d2 + 1;
+    if ( today < expire1a) {
+        alert("the food is not expired");
+        var newDiv = document.createElement("h2");
+        var newContent = document.createTextNode(expire);
+        newDiv.appendChild(newContent); //add the text node to the newly created div.
+        // add the newly created element and its content into the DOM
+        var currentDiv = document.getElementById("foodName1");
+        document.body.insertBefore(newDiv, currentDiv);
+        // alert will only trigger if this month is less than the month put in.
+        // example: april comes before may. (if this month was april and it expired in may)
+    }
+    if (today >= expire1a) {
+        expire1b()
     }
 }
+ function expire1b () {
+     var expire = document.getElementById('expire').value;
+     var expire1b = expire.substring(8,10);
+     var d =  new Date();
+     var today = d.getDate();
+     if(today < expire1b){
+         alert("food is not expired 2");
+         //alert will only trigger if month entered and current month are the same.
+         var newDiv = document.createElement("h3");
+         var newContent = document.createTextNode(expire);
+         newDiv.appendChild(newContent); //add the text node to the newly created div.
+         // add the newly created element and its content into the DOM
+         var currentDiv = document.getElementById("foodName1");
+         document.body.insertBefore(newDiv, currentDiv);
+     }
+     if (today >= expire1b) {
+         alert("food is expired 2");
+         var newDiv = document.createElement("h4");
+         var newContent = document.createTextNode(expire);
+         newDiv.appendChild(newContent); //add the text node to the newly created div.
+         // add the newly created element and its content into the DOM
+         var currentDiv = document.getElementById("foodName1");
+         document.body.insertBefore(newDiv, currentDiv);
 
+     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+ }
 
 
